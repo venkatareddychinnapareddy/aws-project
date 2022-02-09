f363859dcf44888d9b918896697b54073e6e225e
kamal-at-856332198634 <bisht.karan0@gmail.com>
2022-02-09 17:17:36 +0530

    Added rm step in run script

--- myscripts/docker_run.sh
+++ myscripts/docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Running container from pulling image from Amazon ECR..."
+docker rm my-app 
 docker run --name my-app -d -p 80:80 856332198634.dkr.ecr.us-east-2.amazonaws.com/my-app:latest
