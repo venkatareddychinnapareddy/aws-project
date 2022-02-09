ab6a865b573efd34fce4519648561a541dc8d3aa
kamal-at-856332198634 <bisht.karan0@gmail.com>
2022-02-09 17:10:03 +0530

    Changed scripts

--- myscripts/docker_pull.sh
+++ myscripts/docker_pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Pulling from Amazon ECR..."
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+docker pull 856332198634.dkr.ecr.us-east-2.amazonaws.com/my-app:latest
