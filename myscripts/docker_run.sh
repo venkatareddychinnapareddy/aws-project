6d0bc5ee31c18732455fc38572ede5b9d7c4bf27
kamal-at-856332198634 <bisht.karan0@gmail.com>
2022-02-09 17:21:30 +0530

    Changed run scripts

--- myscripts/docker_run.sh
+++ myscripts/docker_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Running container from pulling image from Amazon ECR..."
+docker stop my-app
 docker rm my-app 
 docker run --name my-app -d -p 80:80 856332198634.dkr.ecr.us-east-2.amazonaws.com/my-app:latest
